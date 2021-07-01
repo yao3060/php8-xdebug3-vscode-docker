9d8219f6e490bd754315b5971c015952e255e00b
Yao Yingying <yaoyingying@Dings-Mac-mini.local>
2021-07-01 16:54:19 +0800

    use git commit tag

--- k8s/staging.sh
+++ k8s/staging.sh
@@ -6,4 +6,33 @@ fi
 
 cp overlays/staging/secrets.txt base/apps/secrets.txt
 cp overlays/staging/secrets.txt base/db/secrets.txt
-kubectl apply -k overlays/staging/
+
+FULLSHA=$(git rev-parse HEAD)
+# FULLSHA="0.0.0-${GITSHA:0:8}"
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo "${RED}Set image tags 'MYNEWTAG' to new tag '${FULLSHA}' ${NC}\n"
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  echo "it's $OSTYPE:"
+  sed -i "s/MYNEWTAG/${FULLSHA}/g" ./overlays/staging/kustomization.yaml
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "it's $OSTYPE:"
+  sed -Ei '' "s/MYNEWTAG/${FULLSHA}/g" ./overlays/staging/kustomization.yaml
+else
+  echo "it's unsupport stytem."
+  exit 1
+fi
+
+# test config file
+kubectl kustomize overlays/staging/
+# kubectl apply -k overlays/staging/
+
+echo "Set image tags back to 'MYNEWTAG'"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  sed -i "s/${FULLSHA}/MYNEWTAG/g" ./overlays/staging/kustomization.yaml
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -Ei '' "s/${FULLSHA}/MYNEWTAG/g" ./overlays/staging/kustomization.yaml
+fi
